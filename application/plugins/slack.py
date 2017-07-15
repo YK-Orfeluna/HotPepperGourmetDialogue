01d8d82160b68c0b2738fbc19368b30e9218e4c8
YK-Orfeluna <k030913@kansai-u.ac.jp>
2017-07-15 11:26:46 +0900

    170715

--- application/plugins/slack.py
+++ application/plugins/slack.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+from slackbot.bot import respond_to
 
+@respond_to('こんにちは')
+@respond_to('今日は')
+def hello(message):
+	message.reply('こんにちは!')
+"""
 
+from slackbot.bot import respond_to, listen_to
+
+@listen_to('私は(.*)です')
+@listen_to('わたしは(.*)です')
+def hello(message, something):
+	message.reply('こんにちは!{0}さん。'.format(something))
+
+@respond_to('(.*)')
+def refrection(message, something):
+	message.reply(something)
